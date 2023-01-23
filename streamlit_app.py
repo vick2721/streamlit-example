d8863024e266534b1357108c6cf3c1c7c8328151
vick2721 <88751378+vick2721@users.noreply.github.com>
2023-01-23 13:56:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,34 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+st.title("Time-Series Analysis of Customer Numbers")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Load data
+data = pd.read_csv("customer_data.csv")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Create user interface
+st.sidebar.header("Select Time Range")
+start_date = st.sidebar.date_input("Start date")
+end_date = st.sidebar.date_input("End date")
+chart_type = st.sidebar.selectbox("Select chart type", ["Line", "Bar"])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Filter data based on user input
+filtered_data = data[(data["date"] >= start_date) & (data["date"] <= end_date)]
 
+# Create chart
+if chart_type == "Line":
+    plt.plot(filtered_data["date"], filtered_data["customer_numbers"])
+    plt.xlabel("Date")
+    plt.ylabel("Customer Numbers")
+    st.pyplot()
+else:
+    plt.bar(filtered_data["date"], filtered_data["customer_numbers"])
+    plt.xlabel("Date")
+    plt.ylabel("Customer Numbers")
+    st.pyplot()
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Add export data functionality
+if st.button("Export data"):
+    st.write("Exported data to CSV.")
+    filtered_data.to_csv("filtered_data.csv", index=False)
